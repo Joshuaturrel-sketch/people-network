ce4cbe3e72186d495ff4e8016af52e4ae47053ee
Joshuaturrel-sketch <Joshua.turrel@sfr.fr>
2026-07-16 18:25:36 +0200

    Delete public directory

--- public/config.js
+++
@@ -1,7 +0,0 @@
-export const CONFIG = {
-  API_PEOPLE: "/api/people",
-  API_EDGES: "/api/edges",
-  DEFAULT_THEME: "dark",
-  MAP_TILE_URL: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  MAP_ATTRIBUTION: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a>'
-};
