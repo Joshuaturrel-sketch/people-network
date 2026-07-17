0f5b0c50faaa4805700c430ddf5ecf235218a968
Joshuaturrel-sketch <Joshua.turrel@sfr.fr>
2026-07-17 18:06:03 +0200

    Update notion-config.js

--- notion-config.js
+++ notion-config.js
@@ -1,6 +1,6 @@
 // notion-config.js
 // Keep this file out of version control in real use.
 
-export const NOTION_TOKEN = "your_notion_integration_token_here";
-export const PEOPLE_DB_ID = "your_people_database_id_here";
-export const EDGES_DB_ID = "your_connection_edges_database_id_here";
+export const NOTION_TOKEN = "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";
+export const PEOPLE_DB_ID = "0efd72f587474ff1b9aea3693e9a1848";
+export const EDGES_DB_ID  = "787fed7a9b0e46b3b5068782f14e4b7e";
